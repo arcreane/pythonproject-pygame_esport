131298aa5df10d00514023fa08458ba4c5e8cfe0
Lucas <lucas.lubasinski11@gmail.com>
2020-06-26 11:40:27 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+pygame.init()
+
+
+pygame.display.set_caption("Game")
+window = pygame.display.set_mode((600,385))
+background_image = pygame.image.load('images/background.png')
+
+
+run = True
+
+while run:
+
+    window.blit(background_image, (0, 0))
+
+    pygame.display.flip()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            pygame.quit()
