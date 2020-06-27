6f8ba4a37569837701dd3b08139821729fa9da38
Lucas <lucas.lubasinski11@gmail.com>
2020-06-27 23:12:46 +0200

    text gameOver

--- main.py
+++ main.py
@@ -11,7 +11,17 @@ window.fill((255,0,0))
 background_image = pygame.image.load('images/background.png')
 
 run = True
-
+def creaTexteObj(texte, Police):
+	texteSurface = Police.render(texte, True,white)
+	return texteSurface, texteSurface.get_rect()
+def gameOver():
+    green = (0, 255, 0)
+    blue = (0, 0, 180)
+    Text = pygame.font.Font("Cantarell - Regular.ttf", 150)
+    text_surface_obj = Text.render('GAME OVER', True, green, blue)
+    text_rect_obj = text_surface_obj.get_rect()
+    text_rect_obj.center = (200, 150)
+    window.blit(text_surface_obj)
 
 while run:
 
@@ -22,8 +32,8 @@ while run:
     window.blit(block, (blockPos.x, blockPos.y))
     pygame.display.flip()
 
-    if playerPos.x == monsterPos.x and playerPos.y == 180:
-        Monster.health = 0
+    if playerPos.x == monsterPos.x and playerPos.y == monsterPos.y:
+        gameOver()
 
     elif monsterPos.x != playerPos.x :
         monsterPos.x -= 0.1
